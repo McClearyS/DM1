7acd9792a1c563cb6f3af7e5fa3bfb7fd1a18a57
McClearyS <45406881+McClearyS@users.noreply.github.com>
2022-12-08 12:59:04 -0600

    Update config.h

--- keyboards/handwired/dactyl_manuform/5x6/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/config.h
@@ -1,6 +1,5 @@
 /*
 Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -25,12 +24,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 12
 #define MATRIX_COLS 6
 
+
 // wiring of each half
 #define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
 #define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
 
 #define DIODE_DIRECTION COL2ROW
 #define SOFT_SERIAL_PIN D0
+#define USE_SERIAL
 #define USB_MAX_POWER_CONSUMPTION 500
 
 #define MASTER_RIGHT
+
