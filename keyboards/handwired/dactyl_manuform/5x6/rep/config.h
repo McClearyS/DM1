25a7ac4c319db0e27931495d37f9206ae427d64e
McClearyS <45406881+McClearyS@users.noreply.github.com>
2022-12-09 16:48:53 -0600

    Add files via upload

---
+++ keyboards/handwired/dactyl_manuform/5x6/rep/config.h
@@ -0,0 +1,38 @@
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 12
+#define MATRIX_COLS 6
+
+// wiring of each half
+#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
+#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
+
+#define DIODE_DIRECTION COL2ROW
+#define SOFT_SERIAL_PIN D0
+#define USE_SERIAL
+#define USB_MAX_POWER_CONSUMPTION 500
+
+#define MASTER_RIGHT
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
